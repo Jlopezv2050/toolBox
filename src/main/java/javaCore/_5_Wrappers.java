72cee89f39e59c3c7c8cd6a5c641786576ebbf23
juan <juan.lopez@rocketroi.com>
2017-04-28 14:28:52 +0200

    _5_Wrappers

--- src/main/java/javaCore/_5_Wrappers.java
+++ src/main/java/javaCore/_5_Wrappers.java
@@ -1,50 +1,79 @@
 package javaCore;
 
+import java.awt.*;
+
 /**
  * Created by juan on 21/11/16.
- * JAVA always passes per value
+ * Java is pass-by-value.
+   (OK, once again... with feeling.)
+   Java is pass-by-value.
+   For primitives, you pass a copy of the actual value.
+   For references to objects, you pass a copy of the reference (the remote control).
  */
 public class _5_Wrappers {
 
-    private class ByValue {
+    private class swapPoint {
+        Point firstPoint;
 
-        int value = 0;
+        swapPoint(Point notSwapablePoint){
+            this.firstPoint = notSwapablePoint;
+        }
+    }
 
-        public ByValue(int value){
+        public static void swapWrong(Point arg1, Point arg2)
+        {
+            //arg 1 is a copy from reference of pnt1 so it's pointing to pnt1 and will change it
+            arg1.x = 100;
+            arg1.y = 100;
 
-            this.value = value;
+            //temp will point to the same point that arg1 (pnt1) *
+            Point temp = arg1;
+
+            //arg 1 point to the same point that arg2 (pnt2) *
+            arg1 = arg2;
+
+            //arg2 point to the same point that temp (pnt1) *
+            arg2 = temp;
+
+            //arg 2 is pointing to pnt1
+            arg2.x=89;
+            arg2.y=76;
+            // * point to the same point NOT point to the reference of the operand
         }
 
+    public void swapWithWrapper (swapPoint a, swapPoint b){
+        Point temp = a.firstPoint;
+        a.firstPoint = b.firstPoint;
+        b.firstPoint = temp;
     }
 
-    public ByValue getByValueInstance (int value){
+        public static void main(String [] args)
+        {
 
-        ByValue result = new ByValue(value);
+            Point pnt1 = new Point(1,2);
+            Point pnt2 = new Point(0,0);
+            System.out.println("X: " + pnt1.x + " Y: " +pnt1.y);
+            System.out.println("X: " + pnt2.x + " Y: " +pnt2.y);
+            System.out.println(" ");
+            swapWrong(pnt1, pnt2);
+            System.out.println("X: " + pnt1.x + " Y:" + pnt1.y);
+            System.out.println("X: " + pnt2.x + " Y: " +pnt2.y);
 
-        return result;
+            //Solutions
+              //1. do directly at main method without use a method
+              //2. use wrappers
+            _5_Wrappers aux = new _5_Wrappers();
+
+            swapPoint point1Custom = aux.new swapPoint(pnt1);
+            swapPoint point2Custom = aux.new swapPoint(pnt2);
+            aux.swapWithWrapper(point1Custom, point2Custom);
+            System.out.println("X: " + point1Custom.firstPoint.x + " Y:" + point1Custom.firstPoint.y);
+            System.out.println("X: " + point2Custom.firstPoint.x + " Y: " + point2Custom.firstPoint.y);
+
+
+
+        }
     }
 
-    //even the reference to m1 is passed by value, otherwise we could point from m1 to m2 inside of the method
-    public void metodo_referencia2(ByValue m1) {
-        m1.value = 23;
-        ByValue m2 = new ByValue(1);
-        //we loss the reference
-        m1 = m2;
-        m1.value = 6;
-    }
 
 
-    public static void main (String [] args){
-
-        _5_Wrappers wrappers = new _5_Wrappers();
-
-        ByValue m3 = wrappers.getByValueInstance(2);
-
-        System.out.println(m3.value);
-        wrappers.metodo_referencia2(m3);
-        System.out.println(m3.value);
-
-    }
-
-
-}

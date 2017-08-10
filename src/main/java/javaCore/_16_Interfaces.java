ddbcac5af7c996765b6f5785893ed9889cd7e2cd
juan <juan.lopez@rocketroi.com>
2017-08-10 09:52:25 +0200

    _5_Loops

--- src/main/java/javaCore/_16_Interfaces.java
+++ src/main/java/javaCore/_16_Interfaces.java
@@ -1,7 +1,10 @@
 package javaCore;
 
 /**
- * Created by juan on 24/11/16.
+ * When we talk about abstract classes we are defining characteristics of an object type; specifying what an object is.
+
+ When we talk about an interface and define capabilities that we promise to provide, we are talking about establishing a contract about what the object can do.
+
  */
 public class _16_Interfaces {
 }

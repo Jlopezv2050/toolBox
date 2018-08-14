75c475d32690c7bb8c8c1b064646e70ddb5bb193
juan <juan.lopez@rocketroi.com>
2018-08-14 17:20:27 +0200

    _8_Exceptions tree brief

--- src/main/java/basis/_8_Exceptions.java
+++ src/main/java/basis/_8_Exceptions.java
@@ -3,28 +3,46 @@ package basis;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * "event that disrupts the normal program's flow"
+ *
+ * LBYL
+ * EAFP
+ */
 public class _8_Exceptions {
     int a= 1;
-    //CHECKED (can NOT prevent)
-        //compile time
-        //NO RuntimeException
-        //caught or MUST thrown
 
-        //FileNotFoundException
-        //IOException
-        //SocketException
+    //                                                |  Throwable
+    //                                                |      |
+    //                                                |      |
+    //              ----------------------------------|-------------------------
+    //              |                                 |                        |
+    //              |                                 |                        |
+    //  Error (app shouldn't want to catch/handle)    |                    Exception
+    //         OutOfMemoryError                       |                        |
+    //         StackOverflowError                     |                        |
+    //         VirtualMachineError                    |                        |
+    //         NoClassDefFoundError                   |                        |
+    //                             -------------------|--------------------------------
+    //                             |                  |        |                      |
+    //                      RuntimeException          |  IOException             User defined
+    //                             |                  |  InterruptedException    Exceptions
+    //                             |                  |  TimeOutException
+    //                             |                  |  EOFException
+    //            --------------------------          |  FileNotFoundException
+    //            |                        |          |  ClassNotFoundException
+    //  NullPointerException          User defined    |  SQLException
+    //  IndexOutOfBoundsException   Runtime-Exceptions|
+    //  ArithmeticException                           |
+    //  IllegalArgumentException                      |
+    //                                                |
+    //  UNCHECKED (CAN'T PREVENT ( runtime ),         |  CHECKED( CAN prevent (compile time not runtime),
+    //  CAUGHT or MAY THROWN                          |  CAUGHT OR MUST THROWN)
 
-    //UNCHECKED (CAN prevent)
-        //RuntimeException
-        //caught or MAY thrown
 
-        //ArrayIndexOutOfBoundsException
-        //NullPointerException
-        //IlegalArgumentException
-
-    //errors
-      //stackOverflow
-      //JVM out of memory
+    // WHY NOT CATCH THROWABLE
+    //Throwable covers Error as well and that's usually no point of return. You don't want to catch/handle that,
+    //you want your program to die immediately so that you can fix it properly
 
     protected void testException (){
         try {
@@ -47,6 +65,12 @@ public class _8_Exceptions {
      */
     protected void readFile (){
         try(FileReader fr = new FileReader("\\tmp\\test.txt")){
+
+            try{
+
+            }catch(Exception e){
+
+            }
             char [] a = new char[50];
             fr.read(a);
             for(char c : a)
@@ -59,17 +83,24 @@ public class _8_Exceptions {
     /**
      * Trow user defined Exceptions
      */
-    protected void exceptionMethodThrow () throws _8_b_User_Defined_Exceptions{
-
-        throw new _8_b_User_Defined_Exceptions("MESSAGE");
+    protected void exceptionMethodThrow () throws Exception{
+        try{
+            System.out.println(1/0);
+        } catch (Exception e){
+            throw e;
+        }
 
     }
 
-
     public static void main (String[] args){
-
         _8_Exceptions exceptions = new _8_Exceptions();
         exceptions.testException();
+
+        try {
+            exceptions.exceptionMethodThrow();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }

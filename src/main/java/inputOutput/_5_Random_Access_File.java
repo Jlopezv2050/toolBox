5b93bc8130b3ae20a60e2b8f677803b04f14a268
juan <juan.lopez@rocketroi.com>
2018-09-05 11:50:06 +0200

    _5_Random_Access_File

---
+++ src/main/java/inputOutput/_5_Random_Access_File.java
@@ -0,0 +1,195 @@
+package inputOutput;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Instead reading sequentially (from byte 0 to the end), jump into one. Better use embbeded SQLite.
+ * No read/write_bufferStreams because buffer or stream is a queue (FIFO) and we access randomly.
+ *
+ * FILEPOINTER --> offest where the next R/W will start from.
+ * RECORD --> each set of data
+ * INDEX --> because the length is not fix (ej. 30 bytes/location (n-1)*30) we store the id, offset (startByte)
+ *           and the record length for each record.
+ *
+ * */
+public class _5_Random_Access_File implements Map<Integer, Location> {
+    private static Map<Integer, Location> locations = new LinkedHashMap<>();
+    private static Map<Integer, IndexRecord> index = new LinkedHashMap<>();
+    private static RandomAccessFile ra;
+
+    /**
+     *        | AutoFlush (*) | ModifyDateAtClose |
+     *    ----------------------------------------
+     *  S rw  | YOU TELL      |   YES             |
+     *  L ----------------------------------------
+     *  O rwd | YES           |   YES             |
+     *  W ----------------------------------------
+     *  | rws | YES           |   NO              |
+     *  v ----------------------------------------
+     *
+     *  (*) The commit is not inmediatly, is when the file is close. With autoflush, the synchronisation is handeled.
+     *
+     * */
+    public static void main(String[] args) throws IOException {
+
+        try (RandomAccessFile rao = new RandomAccessFile("locations_rand.dat", "rwd")) {
+            rao.writeInt(locations.size());
+            // location.size() * 3 (locationId + offset + lengthrecord) * byte size
+            int indexSize = locations.size() * 3 * Integer.BYTES;
+            int locationStart = (int) (indexSize + rao.getFilePointer() + Integer.BYTES);
+            rao.writeInt(locationStart);
+            long indexStart = rao.getFilePointer();
+
+            int startPointer = locationStart;
+            //move to specific offset
+            rao.seek(startPointer);
+
+            for(Location location : locations.values()) {
+                rao.writeInt(location.getLocationID());
+                rao.writeUTF(location.getDescription());
+                StringBuilder builder = new StringBuilder();
+                location.getExits().keySet().stream().filter(direction -> !direction.equalsIgnoreCase("Q")).forEach(direction -> {
+                    builder.append(direction);
+                    builder.append(",");
+                    builder.append(location.getExits().get(direction));
+                    builder.append(",");
+                });
+                rao.writeUTF(builder.toString());
+
+                IndexRecord record = new IndexRecord(startPointer, (int) (rao.getFilePointer() - startPointer));
+                index.put(location.getLocationID(), record);
+
+                startPointer = (int) rao.getFilePointer();
+            }
+
+            rao.seek(indexStart);
+            for(Integer locationID : index.keySet()) {
+                rao.writeInt(locationID);
+                rao.writeInt(index.get(locationID).getStartByte());
+                rao.writeInt(index.get(locationID).getLength());
+            }
+
+        }
+
+    }
+
+    // 1. This first four bytes will contain the number of locations (bytes 0-3)
+    // 2. The next four bytes will contain the start offset of the locations section (bytes 4-7)
+    // 3. The next section of the file will contain the index (the index is 1692 bytes long.  It will start at byte 8 and end at byte 1699
+    // 4. The final section of the file will contain the location records (the data). It will start at byte 1700
+
+
+    //We load all the index into memory at the application start because is faster than disk access.
+    static {
+        try {
+            ra = new RandomAccessFile("locations_rand.dat", "rwd");
+            int numLocations = ra.readInt();
+            long locationStartPoint = ra.readInt();
+
+            while(ra.getFilePointer() < locationStartPoint) {
+                int locationId = ra.readInt();
+                int locationStart = ra.readInt();
+                int locationLength = ra.readInt();
+
+                IndexRecord record = new IndexRecord(locationStart, locationLength);
+                index.put(locationId, record);
+            }
+
+        } catch(IOException e) {
+            System.out.println("IOException in static initializer: " + e.getMessage());
+        }
+    }
+
+    public Location getLocation(int locationId) throws IOException {
+
+        IndexRecord record = index.get(locationId);
+        ra.seek(record.getStartByte());
+        int id = ra.readInt();
+        String description = ra.readUTF();
+        String exits = ra.readUTF();
+        String[] exitPart = exits.split(",");
+
+        Location location = new Location(locationId, description, null);
+
+        if(locationId != 0) {
+            for(int i=0; i<exitPart.length; i++) {
+                System.out.println("exitPart = " + exitPart[i]);
+                System.out.println("exitPart[+1] = " + exitPart[i+1]);
+                String direction = exitPart[i];
+                int destination = Integer.parseInt(exitPart[++i]);
+                location.addExit(direction, destination);
+            }
+        }
+
+        return location;
+    }
+
+    @Override
+    public int size() {
+        return locations.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return locations.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return locations.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return locations.containsValue(value);
+    }
+
+    @Override
+    public Location get(Object key) {
+        return locations.get(key);
+    }
+
+    @Override
+    public Location put(Integer key, Location value) {
+        return locations.put(key, value);
+    }
+
+    @Override
+    public Location remove(Object key) {
+        return locations.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Location> m) {
+
+    }
+
+    @Override
+    public void clear() {
+        locations.clear();
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return locations.keySet();
+    }
+
+    @Override
+    public Collection<Location> values() {
+        return locations.values();
+    }
+
+    @Override
+    public Set<Entry<Integer, Location>> entrySet() {
+        return locations.entrySet();
+    }
+
+    public void close() throws IOException {
+        ra.close();
+    }
+
+}

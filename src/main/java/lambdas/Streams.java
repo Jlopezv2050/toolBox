0b1a37a451555819b513bd2b5f7e44352727e4de
jlopezv <juanlv989@gmail.com>
2018-12-12 01:25:19 +0100

    Streams

---
+++ src/main/java/lambdas/Streams.java
@@ -0,0 +1,10 @@
+package lambdas;
+
+public class Streams {
+//Methods references
+//You use lambda expressions to create anonymous methods.
+// Sometimes, however, a lambda expression does nothing but call an existing method.
+// In those cases, it's often clearer to refer to the existing method by name.
+
+    
+}

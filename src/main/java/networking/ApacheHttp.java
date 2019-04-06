189fc4dbf259dd4f6a391c269c2431e0835efde2
jlopezv <juanlv989@gmail.com>
2019-04-06 13:55:06 +0200

    Networking section

---
+++ src/main/java/networking/ApacheHttp.java
@@ -0,0 +1,40 @@
+package networking;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class ApacheHttp {
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://example.org");
+        request.addHeader("User-Agent", "Chrome");
+
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            System.out.println("response code = " + response.getStatusLine().getStatusCode());
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+            String line;
+
+            while ((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            inputReader.close();
+
+        } catch (IOException e) {
+            System.out.println("IOException = " + e.getMessage());
+        }
+
+    }
+
+}
